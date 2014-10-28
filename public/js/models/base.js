3cf8b24c7ebc6430a685ae10e0b4ec24d34ebd8e
Luis A. Echeverria <pixelsnob@gmail.com>
2014-10-27 17:20:26 -0700

    Frontend js stuff, npm deps

--- public/js/models/base.js
+++ public/js/models/base.js
@@ -1,12 +1,31 @@
 /**
- * CMS base model
+ * App base model
  * 
  */
 define([
-  'main/models/base'
-], function(BaseModel) {
-  return BaseModel.extend({
-    
+  'backbone'
+], function(Backbone) {
+  return Backbone.Model.extend({
+    idAttribute: '_id',
+    saved: {},
+    storage: window.localStorage,
+    initialize: function() {
+      // Save a copy fetched from the server for comparisons, etc.
+      this.listenTo(this, 'sync', function(model) {
+        this.saved = _.clone(model.attributes);
+      });
+    },
+    saveLocal: function() {
+      if (typeof this.storage != 'undefined') {
+        this.storage.setItem(this.url, JSON.stringify(this));
+      }
+    },
+    revert: function() {
+      this.set(this.saved);
+    },
+    hasChanged: function() {
+      return !_.isEqual(this.saved, this.attributes);
+    }
   });
 });
 
