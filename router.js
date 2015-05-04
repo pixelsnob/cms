e986de99153a65c4c34eba00fb8b18e3ba857003
Luis A. Echeverria <pixelsnob@gmail.com>
2015-05-03 19:54:00 -0700

    Images and sounds directory tweaks

--- router.js
+++ router.js
@@ -1,38 +1,44 @@
 
-var routes   = require('./routes'),
-    express  = require('express'),
+var express  = require('express'),
     router   = new express.Router();
 
-// Routing
-router.route('/login')
-  .get(routes.loginForm)
-  .post(routes.login);
+module.exports = function(app) {
+  
+  var router = require('./routes')(app);
 
-router.get('/logout', routes.logout);
+  // Routing
+  router.route('/login')
+    .get(routes.loginForm)
+    .post(routes.login);
 
-router.route('/cms/images')
-  .get(routes.auth, routes.getImages)
-  .post(routes.auth, routes.uploadFile, 
-        routes.saveUploadedImage);
+  router.get('/logout', routes.logout);
 
-router.route('/cms/images/:id')
-  .delete(routes.auth, routes.deleteImage)
-  .put(routes.auth, routes.updateImage);
+  router.route('/cms/images')
+    .get(routes.auth, routes.getImages)
+    .post(routes.auth, routes.uploadFile, 
+          routes.saveUploadedImage);
 
-router.route('/cms/sounds')
-  .get(routes.auth, routes.getSounds)
-  .post(routes.auth, routes.uploadFile, 
-        routes.saveUploadedSound);
+  router.route('/cms/images/:id')
+    .delete(routes.auth, routes.deleteImage)
+    .put(routes.auth, routes.updateImage);
 
-router.route('/cms/sounds/:id')
-  .put(routes.auth, routes.updateSound)
-  .delete(routes.auth, routes.deleteSound);
+  router.route('/cms/sounds')
+    .get(routes.auth, routes.getSounds)
+    .post(routes.auth, routes.uploadFile, 
+          routes.saveUploadedSound);
 
-router.route('/cms/pages/:page_id/content_blocks/:content_block_id')
-  .get(routes.auth, routes.getPageContentBlock)
-  .put(routes.auth, routes.savePageContentBlock);
+  router.route('/cms/sounds/:id')
+    .put(routes.auth, routes.updateSound)
+    .delete(routes.auth, routes.deleteSound);
 
-router.route('*').get(routes.renderPage).put(routes.auth, routes.savePage);
+  router.route('/cms/pages/:page_id/content_blocks/:content_block_id')
+    .get(routes.auth, routes.getPageContentBlock)
+    .put(routes.auth, routes.savePageContentBlock);
 
-module.exports = router;
+  router.route('*').get(routes.renderPage).put(routes.auth, routes.savePage);
+
+  return router;
+
+};
+
 
